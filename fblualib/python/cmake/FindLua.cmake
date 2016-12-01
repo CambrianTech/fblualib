34229a0935eb1f38fa4b7d98ec793578dbb7a21d
Joel Teply <joelteply@yahoo.com>
2016-12-01 00:08:53 -0600

    lua

---
+++ fblualib/python/cmake/FindLua.cmake
@@ -0,0 +1,14 @@
+# Find the Lua includes and library
+#
+# LUA_INCLUDE_DIR -- where to find the includes
+# LUA_LIBRARIES -- list of libraries to link against
+# LUA_EXECUTABLE -- lua executable
+# LUAC_EXECUTABLE -- luac executable
+# LUA_FOUND -- set to 1 if found
+
+SET(LUA_FOUND 1)
+SET(LUA_COMPILED_WITH_CXX 1)
+SET(LUA_INCLUDE_DIR "/Users/joelteply/Downloads/torch-ios-ios/exe/lua/src;/Users/joelteply/Downloads/torch-ios-ios/build/exe/lua")
+SET(LUA_LIBRARIES "")
+SET(LUA_EXECUTABLE "lua-static")
+SET(LUAC_EXECUTABLE "torch-luac")
